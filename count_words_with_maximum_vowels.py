241a2d7ec762000f45e72d00617f0473f55f5903
Arthakatlaashajyothi <99869360+Arthakatlaashajyothi@users.noreply.github.com>
2023-05-29 10:37:10 +0530

    def count(n):     p='aeiou'     c=0     for i in n:         if i in p:             c=c+1     return c l=input().lower().split() li=list(l) k=[] for i in li:     k.append(count(i)) t=max(k) for i in k:     print(k.count(i))     break

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,17 @@
+def count(n):
+    p='aeiou'
+    c=0
+    for i in n:
+        if i in p:
+            c=c+1
+    return c
+l=input().lower().split()
+li=list(l)
+k=[]
+for i in li:
+    k.append(count(i))
+t=max(k)
+for i in k:
+    print(k.count(i))
+    break
+    
