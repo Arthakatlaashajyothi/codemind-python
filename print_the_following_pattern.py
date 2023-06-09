b6147c91c5a3a42db1bfafeed213660de451ab9a
Arthakatlaashajyothi <99869360+Arthakatlaashajyothi@users.noreply.github.com>
2023-06-09 06:47:00 +0530

    n=int(input()) for i in range(n,0,-1):    for j in range(n,0,-1):        print(j,end=' ')    print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,14 +1,5 @@
 n=int(input())
-l=[]
-for i in range(1,n+1):
-    l.append(i)
-s=[str(integer) for integer in l]
-string=''.join(s)
-n=int(string)
-p=str(n)
-k=len(p)
-while n:
-    a=n%10**k
-    print(a)
-    n=n//10
-    
+for i in range(n,0,-1):
+   for j in range(n,0,-1):
+       print(j,end=' ')
+   print()
